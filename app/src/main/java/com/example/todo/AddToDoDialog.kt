25982e2f61f01d0cebcf5e4247db06a53a0582bd
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-02-27 13:29:50 -0600

    finished with error handling
    finished with error handling

--- app/src/main/java/com/example/todo/AddToDoDialog.kt
+++ app/src/main/java/com/example/todo/AddToDoDialog.kt
@@ -3,6 +3,7 @@ package com.example.todo
 import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
@@ -20,18 +21,39 @@ class AddToDoDialog(
         val binding = DialogAddTodoBinding.inflate(inflater)
         val builder = AlertDialog.Builder(requireContext())
                 .setView(binding.root)
-                .setPositiveButton(R.string.add){_,_ ->
-                val name = binding.newTodoName.editText?.text?.toString()?:" "
-                val prioritySelected = binding.priorityOptions.checkedRadioButtonId
-                val priority = Priority.from(prioritySelected)
-                val toDo = ToDo(name,priority)
-                onAdd(toDo)
+                .setPositiveButton(R.string.add){ _ , _ ->
+                    onPositiveSelected(binding)
                 }
-                .setNegativeButton(R.string.cancel){_,_ ->
+                .setNegativeButton(R.string.cancel){_ , _ ->
                     //do nothing here
                 }
 
         return builder.create()
+    }
+
+    private fun onPositiveSelected(binding:DialogAddTodoBinding){
+        val name = binding.newTodoName.editText?.text?.toString()?:" "
+        val prioritySelected = binding.priorityOptions.checkedRadioButtonId
+        when{
+            name.isBlank() -> {
+                Toast.makeText(requireContext(),
+                        "Please enter a todo item",
+                        Toast.LENGTH_LONG
+                ).show()
+            }
+            prioritySelected == -1 -> {
+                Toast.makeText(requireContext(),
+                        "Please select a priority",
+                        Toast.LENGTH_SHORT
+                ).show()
+            }
+            else -> {
+                val priority = Priority.from(prioritySelected)
+                val toDo = ToDo(name,priority)
+                onAdd(toDo)
+            }
+        }
+
     }
 
 
